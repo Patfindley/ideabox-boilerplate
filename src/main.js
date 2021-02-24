7b33effca0b439ce933757ca09e52ef17ca335b3
pcmueller <pmuellerleile@gmail.com>
2021-02-24 16:11:33 -0700

    Breathe new life into saveToStorage function

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-customIdea.innerHTML += `
-  <div class="customCard">
-    <nav>
-      <button class="star-button"></button>
-      <button class="delete-button"></button>
-    </nav>
-    <body>
-      <h2>${customCards[i].name}</h2>
-      <img src=${customCards[i].img} />
-    </body>
-    <footer>
-      <button class="comment-button"></button>
-    </footer>
-  </div>
-`
+// customIdea.innerHTML += `
+//   <div class="customCard">
+//     <nav>
+//       <button class="star-button"></button>
+//       <button class="delete-button"></button>
+//     </nav>
+//     <body>
+//       <h2>${customCards[i].name}</h2>
+//       <img src=${customCards[i].img} />
+//     </body>
+//     <footer>
+//       <button class="comment-button"></button>
+//     </footer>
+//   </div>
+// `

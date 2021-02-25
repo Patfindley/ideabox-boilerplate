cc7b7b5ff031182cd35c4f39ea5ff0720998659b
EllieAzaveda <elliemiller66@icloud.com>
2021-02-24 19:11:51 -0700

    Rejuvenate mini cards and add some functionality

--- src/main.js
+++ src/main.js
@@ -1,19 +1,1 @@
-// updateCardDisplay {
-//   customIdea.innerHTML += `
-//     <div class="customCard">
-//       <nav>
-//         <button class="star-button"></button>
-//         <button class="delete-button"></button>
-//       </nav>
-//       <body>
-//         <h2>${customCards[i].name}</h2>
-//         <img src=${customCards[i].img} />
-//       </body>
-//       <footer>
-//         <button class="comment-button"></button>
-//       </footer>
-//     </div>
-//   `
-// }
-
 

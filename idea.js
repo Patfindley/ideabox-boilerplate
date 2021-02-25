85b5f1586ce559d86f226553e29cf8b2e5f5624c
pcmueller <pmuellerleile@gmail.com>
2021-02-24 17:55:03 -0700

    Add basic deleteFromStorage functionality

--- idea.js
+++ idea.js
@@ -3,22 +3,20 @@
 var titleInput = document.getElementById('titleInput');
 var bodyInput = document.getElementById('bodyInput');
 
-// save button
 var saveButton = document.getElementById('saveButton');
+var removeButton = document.getElementById('removeButton');
 
 // global variables
-// array to store user-submitted custom cards (acts as our Data Model)
+
 var customCards = [];
 var currentCard;
 
-// event listening
-
+// event listeners
 
 saveButton.addEventListener('click', saveToStorage);
 
 // class constructor
-//When a card is created, it's an instance of this class
-//Title, Body, save -> Save creates the instance with inputs
+
 class Idea {
   constructor(title, body) {
     this.id = Date.now();
@@ -31,13 +29,29 @@ class Idea {
 
 // functions
 
-// invoked by Save button 'click' event, which calls class constructor using parameters
-// parameters will carry in values of input boxes, also assigned via Save button 'click' event
-// constructor returns instance, stores in currentCard variable, and pushes into customCards array (Data Model)
-
 function saveToStorage() {
   event.preventDefault();
   currentCard = new Idea(titleInput.value, bodyInput.value);
   customCards.push(currentCard);
+
+  // updateCardDisplay;
+
   console.log(currentCard);
+}
+
+// function to splice out card element from customCards array
+
+function deleteFromStorage() {
+  // add listener to 'click' event of "this" remove button's parent element
+  // assign that element's value to removalCard variable
+  var removalCard;
+  // using that removalCard's id, iterate through customCards array and use conditional to match with array index with same id value
+  for (var i = 0; i < customCards.length; i++) {
+    if (customCards[i].id === removalCard.id) {
+      // splice out element
+      customCards.splice(i, 1);
+    }
+  }
+  // run addToCardDisplay to update DOM and refresh page
+  updateCardDisplay;
 }
